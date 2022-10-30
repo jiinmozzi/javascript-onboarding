85875b18fbfc5b49564dd16a7ce85a7ec3ab0b90
jho378 <jho378@naver.com>
2022-10-30 19:41:04 +0900

    problem2 function lists

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,30 @@
 function problem2(cryptogram) {
   var answer;
+  
+  class Node {
+    constructor(data, next = null){
+      this.data = data;
+      this.next = next
+    }
+  }
+  
+  class LinkedList {
+    constructor(){
+      this.head = null;
+    }
+    insertFirst(){}
+    insertLast(){}
+  }
+
+  
+  const generateLinkedList = () => {}
+
+  // remove continuously duplicate alphabets from linkedList 
+  const decode  = () => {}
+
+  // make a whole word from filtered linked list
+  const getAnswer = () => {} 
+
   return answer;
 }
 
